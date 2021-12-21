b61da789dfadb5aa70a9d7712f2fb37fa37ae024
mohammed ilyas <MY_NAME@example.com>
2021-12-21 17:30:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var person =[{
+    name:"spider",
+    age:89,
+},
+{
+    name:"krish",
+    age:34,
+},{
+    name:"jain",
+    age:29,
+}]
+
+console.log(person[name]);
+console.log(person[age]);
